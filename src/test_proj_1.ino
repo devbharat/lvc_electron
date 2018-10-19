8b0de170466c2def90aa3becc010f0e38b9cd6ea
devbharat <bharat.tak8@gmail.com>
2018-10-19 23:56:40 +0200

    Added class to hold cloud variables and function.

--- src/test_proj_1.ino
+++ src/test_proj_1.ino
@@ -6,60 +6,39 @@
  */
 
 #include <ArduinoJson.h>
-#include "Particle.h"
+#include "rpiDataHandler.h"
 
+// Globals
 const size_t READ_BUF_SIZE = 512;
-int led1 = D0;
-int led2 = D7;
-int test_var = 0;
 char readBuf[READ_BUF_SIZE];
 size_t readBufOffset = 0;
 size_t readBufOffset_max = 0;
 size_t readBufOffset_total = 0;
-StaticJsonBuffer<READ_BUF_SIZE> jsonBuffer;
+
+rpiDataHandler handle;
 
 void setup() {
+  /* Register handle to Cloud */
+  handle.initialize();
   Serial.begin(57600);
-
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-
-  Particle.variable("test_var", test_var);
-  char json[] =
-      "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
-  JsonObject& root = jsonBuffer.parseObject(json);
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
-    return;
-  }
-  const char* sensor = root["sensor"];
-  long time = root["time"];
-  double latitude = root["data"][0];
-  double longitude = root["data"][1];
-
-  // Print values.
-  Serial.println(sensor);
-  Serial.println(time);
-  Serial.println(latitude, 6);
-  Serial.println(longitude, 6);
 }
 
+void send_RTH() {
+  StaticJsonBuffer<READ_BUF_SIZE> jsonBuffer;
+  JsonObject &root = jsonBuffer.createObject();
+  root["command"] = "RTH";
+  root.printTo(Serial);
+}
+
+void send_disarm() {
+  StaticJsonBuffer<READ_BUF_SIZE> jsonBuffer;
+  JsonObject &root = jsonBuffer.createObject();
+  root["command"] = "DISARM";
+  root.printTo(Serial);
+}
 
 void loop() {
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-
   delay(1000);
-
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-
-  delay(1000);
-
-  test_var = test_var + 1;
-  Serial.println(test_var);
-  Serial.println(readBufOffset_max);
-  Serial.println(readBufOffset_total);
 
   readBufOffset = 0;
   while(Serial.available()) {
@@ -68,33 +47,52 @@ void loop() {
       if ((c != '\0') ) {
         // Add character to buffer
         readBuf[readBufOffset++] = c;
-        readBufOffset_total++;
-      }
-      else {
+
+      } else {
         // End of line character found, process line
         readBuf[readBufOffset] = 0;
-        readBufOffset_max = readBufOffset;
         processBuffer();
         readBufOffset = 0;
       }
-    }
-    else {
+
+    } else {
       Serial.println("readBuf overflow, emptying buffer");
       readBufOffset = 0;
     }
   }
 
+  if (handle.do_RTH()) {
+      send_RTH();
+
+  } else if (handle.do_disarm()) {
+      send_disarm();
+  }
+
+  // handle.reset();
   // And repeat!
 }
 
 void processBuffer() {
-  StaticJsonBuffer<READ_BUF_SIZE> jsonBuffer1;
-  Serial.printlnf("Received from RPi: %s", readBuf);
-  JsonObject& root1 = jsonBuffer1.parseObject(readBuf);
-  if (!root1.success()) {
+  StaticJsonBuffer<READ_BUF_SIZE> jsonBuffer;
+  // Serial.printlnf("Received from RPi: %s", readBuf);
+  JsonObject& root = jsonBuffer.parseObject(readBuf);
+  if (!root.success()) {
     Serial.printlnf("parseObject() failed %s", readBuf);
     return;
+
   } else {
-    Serial.println("worked elec");
+    // Serial.println("worked elec");
+    if (root.containsKey("lat") && root.containsKey("lng") && root.containsKey("alt")) {
+      handle.parse_position(root["lat"], root["lng"], root["alt"]);
+
+    } else if (root.containsKey("armed_state") && root.containsKey("main_state") && root.containsKey("nav_state")) {
+      handle.parse_vehicle_status(root["armed_state"], root["main_state"], root["nav_state"]);
+
+    } else if (root.containsKey("v_bat") && root.containsKey("cap_bat")) {
+      handle.parse_battery_status(root["v_bat"], root["cap_bat"]);
+
+    } else {
+      Serial.printlnf("Unknown key combination %s", readBuf);
+    }
   }
 }
