af826116da1bd9626f60f5a75476740c1724921e
devbharat <bharat.tak8@gmail.com>
2018-10-29 06:01:09 +0100

    Geofence for bvlos

--- src/geoFence.h
+++ src/geoFence.h
@@ -30,7 +30,7 @@ private:
         bool enable_fence = true;  // Set to false to disable fence
         int id = 1;  // Increment id if you change fence
     } c_1;
-*/
+
     const struct fence_cord_1 {
         // Add your geofence points here
         Vector2l points[MAX_POINTS_GEOFENCE] = {Vector2l(-24304590, 327449480),
@@ -46,6 +46,59 @@ private:
         int num_points = 5;  // Make sure the number match non zero points above
         bool enable_fence = true;  // Set to false to disable fence
         int id = 2;  // Increment id if you change fence
+    } c_1;
+
+
+    const struct fence_cord_1 {
+        // Add your geofence points here
+        Vector2l points[MAX_POINTS_GEOFENCE] = {Vector2l(-24568710,327315760),
+                                            Vector2l(-24493250,327203370),
+                                            Vector2l(-24299450,327376670),
+                                            Vector2l(-24353470,327446030),
+                                            Vector2l(-24568710,327315760),  // make sure the last point is same as first
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0)};
+        int num_points = 5;  // Make sure the number match non zero points above
+        bool enable_fence = true;  // Set to false to disable fence
+        int id = 3;  // Increment id if you change fence
+    } c_1;
+
+    const struct fence_cord_1 {
+        // Add your geofence points here
+        Vector2l points[MAX_POINTS_GEOFENCE] = {Vector2l(-24131370,326975830),
+                                            Vector2l(-24307510,326874930),
+                                            Vector2l(-24523610,327180360),
+                                            Vector2l(-24370970,327458320),
+                                            Vector2l(-24247480,327266150),
+                                            Vector2l(-24131370,326975830),  // make sure the last point is same as first
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0)};
+        int num_points = 6;  // Make sure the number match non zero points above
+        bool enable_fence = true;  // Set to false to disable fence
+        int id = 4;  // Increment id if you change fence
+    } c_1;
+*/
+    // malika to Juma
+    const struct fence_cord_1 {
+        // Add your geofence points here
+        Vector2l points[MAX_POINTS_GEOFENCE] = {Vector2l(-24409310, 329048890),
+                                            Vector2l(-24188060, 327358790),
+                                            Vector2l(-24218740, 327358310),
+                                            Vector2l(-24436450, 329038410),
+                                            Vector2l(-24409310, 329048890),  // make sure the last point is same as first
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0),
+                                            Vector2l(0, 0)};
+        int num_points = 5;  // Make sure the number match non zero points above
+        bool enable_fence = true;  // Set to false to disable fence
+        int id = 5;  // Increment id if you change fence
     } c_1;
 
     Vector2l current_pos;
